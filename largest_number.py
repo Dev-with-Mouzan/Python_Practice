bdd06db7c19cd5997aaa0a7760ed0e50ab98dc87
Mouzan Raza <mouzan.ai.dev@gmail.com>
2025-12-07 11:04:50 +0500

    day 1 practice

---
+++ largest_number.py
@@ -0,0 +1,9 @@
+a=int(input("Enter First number: "))
+b=int(input("Enter Second number: "))
+c=int(input("Enter Third number: "))
+if a>b & a>c:
+    print("The greater Number is : ",a)
+elif b>a & b>c:
+    print("The Greater Number is: ",b)
+else:
+    print("The Greater Number is: ",c)
