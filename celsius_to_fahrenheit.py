67dae9fbf340d3a497297fae23cdb20e2f51cf13
Mouzan Raza <mouzan.ai.dev@gmail.com>
2026-01-15 21:35:12 +0500

    updated

--- celsius_to_fahrenheit.py
+++ celsius_to_fahrenheit.py
@@ -1,10 +1,4 @@
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-
-if __name__ == "__main__":
-    try:
-        c = float(input("Enter temperature in Celsius: "))
-        f = celsius_to_fahrenheit(c)
-        print(f"{c}°C is equal to {f}°F")
-    except ValueError:
-        print("Please enter a valid number.")
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+print(f"{c}°C is equal to {f}°F")
+   
